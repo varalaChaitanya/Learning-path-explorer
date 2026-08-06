fa0c87ff6cfdfbee7885c7827119dc83643f79b9
Chaitanya Reddy <144891412+varalaChaitanya@users.noreply.github.com>
2026-08-06 12:00:33 +0530

    Refactor CORS setup in app.js

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ const instructorRoutes = require("./routes/instructorRoutes")
 const app = express()
 
 app.use(cors())
+
+app.use(
+  cors({
+    origin: "*",
+  })
+)
+
 app.use(express.json())
 
 app.get("/", (req, res) => {
